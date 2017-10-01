4dd77f39981a5b43929b37eb755c90551787a69a
obabichev <Aluckard.13>
2017-10-01 11:19:28 +0300

    init

---
+++ src/com/obabichev/TestServlet.java
@@ -0,0 +1,21 @@
+package com.obabichev;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by obabichev on 01/10/2017.
+ */
+@WebServlet("/")
+public class TestServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().append("Hello!").append(request.getContextPath());
+    }
+}
