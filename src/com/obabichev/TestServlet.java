c0b81683256963e7e313f084d4e10fad0f323c94
obabichev <Aluckard.13>
2017-10-29 10:08:09 +0300

    VIDEO 1 (END)

--- src/com/obabichev/TestServlet.java
+++ src/com/obabichev/TestServlet.java
@@ -6,16 +6,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * Created by obabichev on 01/10/2017.
- */
+
 @WebServlet("/")
 public class TestServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().append("Hello!").append(request.getContextPath());
+        response.getWriter()
+                .append("Hello!!!!")
+                .append(request.getContextPath());
     }
 }
