4e52ce61a7d702c2a2c49541bc384065c4789145
obabichev <Aluckard.13>
2017-12-03 14:28:25 +0300

    VIDEO 2 (END)

--- src/com/obabichev/todotree/domain/User.java
+++ src/com/obabichev/todotree/domain/User.java
@@ -1,7 +1,20 @@
 package com.obabichev.todotree.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "_USER")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "LOGIN", unique = true, nullable = false)
     private String login;
+
+    @Column(name = "PASSWORD", nullable = false)
     private String password;
 
     public String getLogin() {
@@ -19,5 +32,5 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
-    
+
 }
---
+++ src/com/obabichev/todotree/service/UserService.java
@@ -0,0 +1,10 @@
+package com.obabichev.todotree.service;
+
+import com.obabichev.todotree.domain.User;
+
+public class UserService {
+
+    public User create(User user) {
+        return null;
+    }
+}
